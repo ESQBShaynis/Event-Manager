42aa51dd420b822ffc4b037e0dc3c3bf49877540
haynis <75485602+haynis@users.noreply.github.com>
2020-12-16 22:57:22 +0700

    Update login.php

--- appweb/login.php
+++ appweb/login.php
@@ -55,3 +55,42 @@ if ( isset($_POST["login"]) ) {
 	}
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Login</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="assets/gaya.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	</head>
+
+	<body>
+		<div class="card altaboutus altcard" id="aboutus">
+		  <center><div class="card-body alteraboutus">
+		    <section>
+		    	<form action="" method="post">
+		    	  <div class="form-group">
+		    	    <label for="username">Username</label>
+		    	    <input type="text" class="form-control" name="username" id="username">
+		    	  </div>
+
+		    	  <div class="form-group">
+		    	    <label for="exampleInputPassword1">Password</label>
+		    	    <input type="password" class="form-control" name="password" id="password">
+		    	  </div>
+
+		    	  <div class="form-group">
+		    	  	<input type="checkbox" name="ingat" id="ingat">
+		    	  	<label for="ingat"> Ingat saya</label>
+		    	  </div>
+		    	  <button type="submit" class="btn btn-primary" name="login">Login</button>
+		    	</form>
+		    </section>
+		  </div></center>
+		</div>
+	</body>
+</html>
